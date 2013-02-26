990273dd64ba38545c475ba0c0336f884b5154e5
Joshua Sierles <joshua@diluvia.net>
2013-02-26 16:13:29 -0600

    add profiler in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  before_filter do
+  before_filter :profiler
+  
+  def profiler
     Rack::MiniProfiler.authorize_request
   end
   
