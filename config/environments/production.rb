c456015be50717aa770e18fd167fb72bb88af6b1
Joshua Sierles <joshua@diluvia.net>
2013-02-27 11:14:37 -0600

    disable racke cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,9 +19,9 @@ CoolSpots::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  config.action_dispatch.rack_cache =  {:metastore=>"rails:/",
-                                        :entitystore=>"rails:/",
-                                        :verbose=>true}
+  #config.action_dispatch.rack_cache =  {:metastore=>"rails:/",
+  #                                      :entitystore=>"rails:/",
+  #                                      :verbose=>true}
   
 
   config.assets.compress = true
